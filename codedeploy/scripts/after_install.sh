1f7f9de0a7a198d1c0d00b42f7320fa78c99e920
2anoopp <2anoopp@gmail.com>
2019-12-08 17:42:02 +0530

    Create after_install.sh

---
+++ codedeploy/scripts/after_install.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+# CodeDeploy Application life-cycle 3 : Deploy Application 
+
+# Deploy latest revision to production
+cp /opt/glues/java-tomcat-maven-example.war /var/lib/tomcat9/webapps/glues.war
